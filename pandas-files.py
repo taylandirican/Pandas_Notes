78835fa8d47f079a2fc4e980509dcbbaf70189de
Taylan Dirican <ertaylan680@gmail.com>
2026-09-14 15:58:56 +0300

    A note on the Pandas sample files has been added

---
+++ pandas-files.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# df = pd.read_csv("sample.csv")  #; olmamalı - , olmalı
+# df = pd.read_json("sample2.json", encoding="utf-8")
+df = pd.read_excel("sample.xlsx")
+
+print(df)
